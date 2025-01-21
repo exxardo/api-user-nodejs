69a07e2068e3fb94fe89427af103704a5d9e3814
Eduardo Ramiro <73205966+exxardo@users.noreply.github.com>
2025-01-21 20:37:04 -0300

    Finalização do projeto (#2)
    * configuraçao de rotas
    
    * conexão com postgres docker
    
    * COrreçao na conexao com o banco de dados
    
    * Finalizaçao do projeto
    
    * Finalizaçao do projeto
    
    * Finalizaçao do projeto

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,9 @@
+/**
+ * Arquivo de configuração do Sequelize para conectar ao banco de dados PostgreSQL.
+ *
+ * O Sequelize é um ORM (Object-Relational Mapper) para Node.js que facilita a interação com o banco de dados.
+ * Este arquivo configura as informações de conexão com o banco de dados PostgreSQL e define algumas opções de estilo de nomenclatura.
+ */
 export default {
     dialect: 'postgres',
     host: 'localhost',
