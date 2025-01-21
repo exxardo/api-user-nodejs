cd77b89c10a1734759bba3427849790f2b7bf468
exxardo <eduardoramirodematos@gmail.com>
2025-01-21 19:50:13 -0300

    Finalizaçao do projeto

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,26 @@
-export const createUser = () => (req, res) => {
+import User from '../models/User.js';
+import httpStatusCode from '../config/httpStatusCodes.js';
+import crypto from 'node:crypto';
 
-    res.status(200).json({message:"Cadastrado com sucesso"})
 
-}
+// Criar usuário
+export const createUser = async (req, res) => {
+    try {
+        const userToCreate = {
+            id: crypto.randomUUID(),
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email,
+        };
 
+        // Criação do usuário com o objeto correto
+        const user = await User.create(userToCreate);
+
+        res.status(httpStatusCode.CREATED).json(user);
+    } catch (error) {
+        res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ error: error.message });
+    }
+};
 export const getAllUser = () => (req, res) => {
 
     res.status(200).json({message:"Cadastrado com sucesso"})
